7fba7986836d0d5f2a868a941362255492816e37
projectcargoswift <main.cargoswift@gmail.com>
2025-05-18 20:22:55 +0200

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify  
 
-app = Flask(__name__)  
+app = Flask(__name__, static_folder='packages/webconsole/static')  # Ujistěte se, že je správná cesta  
 
 @app.route('/')  
 def index():  
@@ -9,11 +9,13 @@ def index():
 @app.route('/command', methods=['POST'])  
 def command_endpoint():  
     command = request.json.get('command')  
-    # Zpracování příkazu  
+    # Zpracování příkazu (můžete přidat vlastní logiku zde)  
     response_message = f"Received command: {command}"  
-    # Tady by mělo být volání ke zpracování příkazu na straně SwiftOS  
     return jsonify({"message": response_message})  
 
 def start_web_server():  
     """Spouští webový server."""  
-    app.run(host='0.0.0.0', port=8001, use_reloader=False)
+    app.run(host='0.0.0.0', port=8001, use_reloader=False)  
+
+if __name__ == "__main__":  
+    start_web_server()
