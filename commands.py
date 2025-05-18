65d41d2e55f21921825ce17ed9e5f012e2ecbf40
projectcargoswift <main.cargoswift@gmail.com>
2025-05-18 20:16:33 +0200

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,5 +1,6 @@
-from .webserver import start_web_server  # Použijte relativní import  
+from webconsole.webserver import start_web_server  # Absolutní import
 
 def register_commands(command_registry):  
     """Registruje příkazy pro spuštění webové konzole."""  
     command_registry['start-web-console'] = start_web_server
+
