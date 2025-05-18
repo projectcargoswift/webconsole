bad717a75dd49b1ded13787214080f3f7ef8731b
projectcargoswift <main.cargoswift@gmail.com>
2025-05-18 19:40:55 +0200

    Update web_console.py

--- web_console.py
+++ web_console.py
@@ -1,7 +1,6 @@
 # /files/packages/webconsole/web_console.py  
 
 from flask import Flask, request, render_template  
-import threading  
 import subprocess  
 
 app = Flask(__name__)  
@@ -17,17 +16,11 @@ def execute_command():
     command = request.form.get('command')  
     console_output.append(f"> {command}")  # Uchování příkazu  
 
-    # Zde můžete spustit příkaz v rámci vašeho operačního systému  
+    # Zpracování příkazu a uchování výstupu  
     try:  
         result = subprocess.check_output(command, shell=True, universal_newlines=True)  
         console_output.append(result)  
     except subprocess.CalledProcessError as e:  
         console_output.append(f"Error: {e}")  
 
-    return render_template('index.html', output=console_output)  
-
-def run_server():  
-    app.run(host='0.0.0.0', port=5000)  
-
-def start_web_console():  
-    threading.Thread(target=run_server).start()
+    return render_template('index.html', output=console_output)
